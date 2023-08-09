2108ad776203f23183132ee0b27d4ffb38e77e3d
mariobassem <mariobassem12@gmail.com>
2023-08-09 08:49:08 +0300

    simple sql compiler and virtual machine
    Signed-off-by: mariobassem <mariobassem12@gmail.com>

--- cmds/main.go
+++ cmds/main.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+const (
+	StatementInsert = iota
+	StatementSelect
+)
+
+type Statement struct {
+	typ int
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -16,12 +25,64 @@ func main() {
 			log.Fatal(err)
 		}
 
-		switch scanner.Text() {
-		case ".exit":
-			os.Exit(0)
-		default:
-			fmt.Printf("Unrecognized command %s.\n", scanner.Text())
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		if line[0] == '.' {
+			exit, err := doMetaCommand(line)
+			if err != nil {
+				fmt.Printf("meta error: %s\n", err.Error())
+			}
+
+			if exit {
+				os.Exit(0)
+			}
+
+			continue
+		}
+
+		statement, err := prepareStatement(line)
+		if err != nil {
+			fmt.Printf("sql error: %s\n", err.Error())
+			continue
+		}
+
+		if err := executeStatement(statement); err != nil {
+			fmt.Printf("exec error: %s\n", err.Error())
 		}
 	}
 
+}
+
+func doMetaCommand(command string) (exit bool, err error) {
+	switch command {
+	case ".exit":
+		return true, nil
+	default:
+		return false, fmt.Errorf("Unrecognized command %s", command)
+	}
+}
+
+func prepareStatement(command string) (Statement, error) {
+	switch command {
+	case "insert":
+		return Statement{typ: StatementInsert}, nil
+	case "select":
+		return Statement{typ: StatementSelect}, nil
+	default:
+		return Statement{}, fmt.Errorf("unrecognized statement %s", command)
+	}
+}
+
+func executeStatement(statement Statement) error {
+	switch statement.typ {
+	case StatementInsert:
+		fmt.Printf("this is where an insert is handled")
+	case StatementSelect:
+		fmt.Print("this is where a select is handled")
+	}
+
+	return nil
 }
